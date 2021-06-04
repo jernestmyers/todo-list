0faac9f19ea1ba94bc337b7950ef21d95ea9f416
jernestmyers <inernestmusic@gmail.com>
2021-06-04 13:54:28 -0500

    added navigation buttons and buttons to add task/project to the left container

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -5,13 +5,50 @@ function loadHeader(container) {
 }
 
 function loadLeftBar(container) {
-    const addTask = document.createElement(`p`);
-    const addProject = document.createElement(`p`);
-    addTask.textContent = `add task`;
-    addProject.textContent = `add project`;
+    // for the add project and add task buttons
+    const addItemsContainer = document.createElement(`div`);
+    addItemsContainer.setAttribute(`id`, `add-task-container`);
 
-    container.appendChild(addProject);
-    container.appendChild(addTask);
+    const addTask = document.createElement(`button`);
+    addTask.textContent = `+ add task`;
+    addTask.classList.add(`buttonToAddItem`);
+    addTask.addEventListener(`click`, (e) => {console.log(`task`)});
+    addItemsContainer.appendChild(addTask);
+
+    const addProject = document.createElement(`button`);
+    addProject.textContent = `+ add project`;
+    addProject.classList.add(`buttonToAddItem`);
+    addProject.addEventListener(`click`, (e) => {console.log(`project`)});
+    addItemsContainer.appendChild(addProject);
+
+    container.appendChild(addItemsContainer);
+
+    // for the navigation buttons
+    const containerForNavigation = document.createElement(`div`);
+    containerForNavigation.setAttribute(`id`, `nav-container`);
+    containerForNavigation.addEventListener(`click`, (e) => console.log(e.target.textContent));
+    
+    const overviewButton = document.createElement(`button`);
+    overviewButton.textContent = `overview`;
+    containerForNavigation.appendChild(overviewButton);
+
+    const todayButton = document.createElement(`button`);
+    todayButton.textContent = `today`;
+    containerForNavigation.appendChild(todayButton);
+
+    const weeklyButton = document.createElement(`button`);
+    weeklyButton.textContent = `this week`;
+    containerForNavigation.appendChild(weeklyButton);
+
+    const overdueButton = document.createElement(`button`);
+    overdueButton.textContent = `past due`;
+    containerForNavigation.appendChild(overdueButton);
+
+    const projectsButton = document.createElement(`button`);
+    projectsButton.textContent = `projects`;
+    containerForNavigation.appendChild(projectsButton);
+
+    container.appendChild(containerForNavigation);
 }
 
 function loadMainContent(container) {
