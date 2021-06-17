477333d6deedb60efbdad3cad7a921719ed841db
Jeremy Myers <inernestmusic@gmail.com>
2021-06-17 13:15:24 -0500

    update index.js to the majorRefactor version.

--- src/index.js
+++ src/index.js
@@ -24,104 +24,6 @@ function pageSelector(e) {
 }
 
 function projectSelector(e) {
-<<<<<<< HEAD
-    const projectSelected = e.target.textContent;
-    regenerateProjectTasks(projectSelected);
-}
-
-function regenerateProjectTasks(pageTitle) {
-    console.log(`regenerateTasks`);
-    const currentObjectArray = getObjectArrays();
-    if (pageTitle === `overview`) {
-        loadMainContent(mainContainer, displayTasksOverview(currentObjectArray.tasks));
-    } else {
-        const projectDisplayedObject = currentObjectArray.projects.filter( object => {
-            if (object.projectTitle === pageTitle) {
-                return object;
-            }
-        })
-        const filterTasks = currentObjectArray.tasks.filter( object => {
-            if (object.projectAssociated === pageTitle) {
-                return object
-            }
-        })
-        console.log(projectDisplayedObject[0]);
-        loadMainContent(mainContainer, displayExistingProject(projectDisplayedObject[0], filterTasks));
-    }
-}
-
-const createTaskAndProjectModule = (function() {
-
-    const projectUserInput = document.querySelectorAll(`.projectUserInputs`);
-    const taskUserInput = document.querySelectorAll(`.taskUserInputs`);
-
-    function checkFormValidation(inputNodeList) {
-        let isValid = true;
-        inputNodeList.forEach( inputField => {
-            if (inputField.validity.valueMissing) {
-                isValid = false;
-            }
-        })
-        return isValid
-    }
-
-    function instantiateNewProject() {
-        const projectInputArray = Array.from(projectUserInput);
-        const currentObjectArray = getObjectArrays();
-        const projectIndex = currentObjectArray.projects.length;
-        createNewProject(projectInputArray[0].value, projectInputArray[1].value, projectInputArray[2].value, projectIndex);
-        loadMainContent(mainContainer, displayNewProject(currentObjectArray.projects[projectIndex]));
-        appendNewProjectToSelector(projectInputArray[0].value);
-        attachDataToProjectButton(projectIndex);
-    }
-
-    function attachDataToProjectButton(indexNumber) {
-        const newProjectButton = projectListContainer.lastChild;
-        newProjectButton.setAttribute(`data-index-number`, indexNumber);
-    }
-
-    function appendNewProjectToSelector(newProjectTitle) {
-        const addTaskProjectSelector = document.querySelector(`#project-associated`);
-        const editTaskProjectSelector = document.querySelector(`#edit-project-associated`);
-        
-        const projectForAddTaskSelector = document.createElement(`option`);
-        projectForAddTaskSelector.setAttribute(`value`, newProjectTitle);
-        projectForAddTaskSelector.textContent = newProjectTitle;
-        
-        const projectForEditTaskSelector = document.createElement(`option`);
-        projectForEditTaskSelector.setAttribute(`value`, newProjectTitle);
-        projectForEditTaskSelector.textContent = newProjectTitle;
-
-        addTaskProjectSelector.appendChild(projectForAddTaskSelector);
-        editTaskProjectSelector.appendChild(projectForEditTaskSelector);
-    }
-
-    function instantiateNewTask() {
-        const taskInputArray = Array.from(taskUserInput);
-        const currentPageDisplayed = mainContainer.firstChild.firstChild.textContent;
-        createNewTask(taskInputArray[0].value, taskInputArray[1].value, taskInputArray[2].value, taskInputArray[3].value, taskInputArray[4].value);
-        const currentObjectArray = getObjectArrays();
-        const newTaskIndex = currentObjectArray.tasks.length - 1;
-        if (currentPageDisplayed === `overview`) {
-            loadMainContent(mainContainer, displayTasksOverview(currentObjectArray.tasks));
-        } else if (currentPageDisplayed === currentObjectArray.tasks[newTaskIndex].projectAssociated) {
-            regenerateProjectTasks(currentPageDisplayed);
-        }
-    }
-
-    const submitProjectButton = document.querySelector(`#addProjectSubmitButton`);
-    const submitTaskButton = document.querySelector(`#addTaskSubmitButton`);
-    const cancelProjectButton = document.querySelector(`#cancelProject`);
-    const cancelTaskButton = document.querySelector(`#cancelTask`);
-
-    cancelProjectButton.addEventListener(`click`, (e) => closeFormModal(e.target.id));
-    cancelTaskButton.addEventListener(`click`, (e) => closeFormModal(e.target.id));
-
-    submitProjectButton.addEventListener(`click`, (e) => {
-        if (checkFormValidation(projectUserInput)) {
-            instantiateNewProject();
-            submitForm(e);
-=======
     const currentObjectArray = getObjectArrays();
     const projectClickedTitle = e.target.textContent;
     const projectClickedIndex = e.target.dataset.indexNumber;
@@ -130,7 +32,6 @@ const createTaskAndProjectModule = (function() {
     currentObjectArray.tasks.filter( (taskObject) => {
         if (taskObject.taskProjectAssociated === projectClickedTitle) {
             associatedTasksToLoad.push(taskObject);
->>>>>>> majorRefactor
         }
     })
 
